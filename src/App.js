8d58cb6dae0eef27656d771a5f791da77a722378
williamsadugyamfi <aduyamfiwilliams23@mail.com>
2026-07-23 08:25:04 +0000

    rendering the items list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 2, packed: true },
+];
+
 const App = function () {
   return (
     <div className="app">
@@ -16,13 +22,33 @@ const Logo = function () {
 const Form = function () {
   return (
     <div className="add-form">
-      <h3>Whta do you need for your trip😍</h3>
+      <h3>What do you need for your trip😍</h3>
     </div>
   );
 };
 
 const ParkingList = function () {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = function ({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = function () {
